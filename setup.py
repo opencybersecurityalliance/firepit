423c99490e7bbea6eea7161f11112cc28c50a07a
Paul Coccoli <pcoccoli@us.ibm.com>
2021-05-19 08:53:48 -0400

    Correct project URL in setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/pcoccoli/firepit',
+    url='https://github.com/IBM/firepit',
     version='1.0.0',
     zip_safe=False,
 )
