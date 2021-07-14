67958a19aa30ac7ba46bb6dd60089b5b7bbbe3f1
Paul Coccoli <pcoccoli@us.ibm.com>
2021-07-14 14:12:53 -0400

    Bump version: 1.0.13 → 1.0.14

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/IBM/firepit',
-    version='1.0.13',
+    version='1.0.14',
     zip_safe=False,
 )
