b7a156251b9d8903f998fb2c031bd56e48c4479a
Paul Coccoli <pcoccoli@us.ibm.com>
2022-03-30 14:44:05 -0400

    Doc updates; housekeeping for 2.0 release

--- setup.py
+++ setup.py
@@ -28,14 +28,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="IBM Security",
     author_email='pcoccoli@us.ibm.com',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
