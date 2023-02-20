1fb42bc8934e7282a9f34cc23f6b91a07c81e992
Paul Coccoli <pcoccoli@us.ibm.com>
2023-02-20 17:13:20 -0500

    Bump version: 2.3.11 → 2.3.12

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/opencybersecurityalliance/firepit',
-    version='2.3.11',
+    version='2.3.12',
     zip_safe=False,
 )
