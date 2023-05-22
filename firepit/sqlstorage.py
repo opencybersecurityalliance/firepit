d8fbcaa1c43be5a1a1cd05941ac10af7b3c026e2
leila-rashidi <leila.rashidi.lr@gmail.com>
2023-05-22 18:20:23 -0300

    Update sqlstorage.py

--- firepit/sqlstorage.py
+++ firepit/sqlstorage.py
@@ -899,7 +899,21 @@ class SqlStorage:
         else:
             count = self.count(viewname)
         return count
-        
+    
+    def timestamped(
+            self,
+            viewname,
+            path=None,
+            value=None,
+            timestamp='first_observed',
+            limit=None,
+            run=True):
+        """
+        Get the timestamped observations of `value` in `viewname`.`path`
+        Returns list of dicts like {'timestamp': '2021-10-...', '{column}': '...'}
+        """
+        return extract_observeddata_attribute(viewname, timestamp)
+    
     def extract_observeddata_attribute(
             self,
             viewname,
