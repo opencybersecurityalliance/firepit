ccfc25c945e5e4321a418f8c0cf59715db332958
Paul Coccoli <pcoccoli@us.ibm.com>
2023-07-05 13:41:22 -0400

    Flatten extension properties on custom objects

--- tests/test_raft.py
+++ tests/test_raft.py
@@ -11,6 +11,7 @@ from firepit.raft import json_normalize
          {'type': 'file', "hashes.'SHA-1'": 'abc123'}),
         ({'type': 'foo', 'a': {'b': {'c': 1, 'd': 2}}}, {'type': 'foo', 'a.b.c': 1, 'a.b.d': 2}),
         ({'type': 'x-foo', 'a': {'b': {'c': 1, 'd': 2}}}, {'type': 'x-foo', 'a': {'b': {'c': 1, 'd': 2}}}),
+        ({'type': 'x-foo', 'extensions': { 'x-cool-ext': {'a': 1, 'b': 2}}}, {'type': 'x-foo', "extensions.'x-cool-ext'.a": 1, "extensions.'x-cool-ext'.b": 2}),
     ]
 )
 def test_flatten_complex(obj, expected):
